c245a87e0c9cf5a4b670ccfc772ec94580907e61
udaychaitanya <udaychaitanya011@gmail.com>
2026-08-02 16:08:30 +0530

    Payment Service Changes

--- LoadBridge/frontend/app/src/features/payments/services/paymentService.js
+++
@@ -1,9 +0,0 @@
-import apiClient from '../../../shared/services/api';
-
-export const createOrderApi = (payload) => {
-  return apiClient.post('/payments/create-order', payload).then((response) => response.data);
-};
-
-export const verifyPaymentApi = (payload) => {
-  return apiClient.post('/payments/verify', payload).then((response) => response.data);
-};
